9f76dd0bcba49dd8201cc46993d93a182a588f9d
Nikolaj Tost Søgaard <cph-ns169@cphbusiness.dk>
2019-09-11 14:00:32 +0200

    Changes for task 1 tests

--- src/test/java/facades/MembersFacadeTest.java
+++ src/test/java/facades/MembersFacadeTest.java
@@ -1,7 +1,10 @@
 package facades;
 
+import dto.MembersDTO;
 import utils.EMF_Creator;
 import entities.Members;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.AfterAll;
@@ -20,49 +23,48 @@ public class MembersFacadeTest {
 
     private static EntityManagerFactory emf;
     private static MembersFacade facade;
+    public static Members member;
+    public static List<Members> members = new ArrayList<>();
 
     public MembersFacadeTest() {
     }
 
     //@BeforeAll
     public static void setUpClass() {
-        emf = EMF_Creator.createEntityManagerFactory(
-                "pu",
-                "jdbc:mysql://localhost:3307/startcode_test",
-                "dev",
-                "ax2",
-                EMF_Creator.Strategy.CREATE);
-        facade = MembersFacade.getFacadeExample(emf);
+//        emf = EMF_Creator.createEntityManagerFactory(
+//                "pu",
+//                "jdbc:mysql://localhost:3307/startcode_test",
+//                "dev",
+//                "ax2",
+//                EMF_Creator.Strategy.CREATE);
+//        facade = MembersFacade.getMembersFacade(emf);
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST,Strategy.DROP_AND_CREATE);
     }
 
-    /*   **** HINT **** 
-        A better way to handle configuration values, compared to the UNUSED example above, is to store those values
-        ONE COMMON place accessible from anywhere.
-        The file config.properties and the corresponding helper class utils.Settings is added just to do that. 
-        See below for how to use these files. This is our RECOMENDED strategy
-     */
     @BeforeAll
     public static void setUpClassV2() {
        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST,Strategy.DROP_AND_CREATE);
-       facade = MembersFacade.getFacadeExample(emf);
+       facade = MembersFacade.getMembersFacade(emf);
     }
 
     @AfterAll
     public static void tearDownClass() {
-//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
     }
 
     // Setup the DataBase in a known state BEFORE EACH TEST
     //TODO -- Make sure to change the script below to use YOUR OWN entity class
     @BeforeEach
     public void setUp() {
+        facade = MembersFacade.getMembersFacade(emf);
+        
+        member = new Members("N", "T", "Yellow", "nt@cphbusiness.dk");
+        members.add(member);
+        
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
-            em.persist(new Members("Some txt", "More text"));
-            em.persist(new Members("aaa", "bbb"));
-
+            em.createNativeQuery("DELETE FROM MEMBERS").executeUpdate();
+            em.persist(member);
             em.getTransaction().commit();
         } finally {
             em.close();
@@ -71,13 +73,72 @@ public class MembersFacadeTest {
 
     @AfterEach
     public void tearDown() {
-//        Remove any data after each test was run
     }
 
     // TODO: Delete or change this method 
     @Test
-    public void testAFacadeMethod() {
-        assertEquals(2, facade.getRenameMeCount(), "Expects two rows in the database");
+    public void testGetMembersCount() {
+        Long expected = 1l;
+        Long result = facade.getAllMembersCount();
+        assertEquals(expected, result);
     }
-
+    
+    @Test
+    public void testGetAllMembers(){
+        List<MembersDTO> expected = new ArrayList<>();
+        expected.add(new MembersDTO(member));
+        
+        List<MembersDTO> result = facade.getAllMembers();
+        
+        assertEquals(expected, result);
+    }
+    
+    // Not sure if it works
+    @Test
+    public void testGetMembersByID(){
+        Long expected = new MembersDTO(member).getId();
+        
+        Long result = member.getId();
+        
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testGetMembersByColor(){
+        List<MembersDTO> expected = new ArrayList<>();
+        expected.add(new MembersDTO(member));
+        
+        List<MembersDTO> result = facade.getMembersByColor("Yellow");
+        
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testGetMembersByEmail(){
+        String expected = new MembersDTO(member).getEmail();
+        
+        String result = member.getEmail();
+        
+        assertEquals(expected, result);
+    }
+    
+    @Test 
+    public void testGetAddMember(){
+        
+        Members newM = new Members("M", "B", "Yellow", "");
+        
+        Members result = facade.addMember(newM);
+        
+        assertEquals(newM, result);
+        
+        EntityManager em = emf.createEntityManager();
+        try{
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.find(Members.class, new Long(members.size()+1)));
+            em.getTransaction().commit();
+        } finally{
+            em.close();
+        }
+    }
 }
